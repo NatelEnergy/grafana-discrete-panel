716bf4c7161b049887f596e58470968adcddc17d
Ryan McKinley <ryantxu@gmail.com>
2018-08-08 13:45:02 -0700

    use ISO date

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const webpack = require('webpack');
-const moment = require('moment');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ReplaceInFileWebpackPlugin = require('replace-in-file-webpack-plugin');
@@ -64,7 +63,7 @@ module.exports = {
           },
           {
             search: '%TODAY%',
-            replace: moment().format('YYYY.MM.DD'),
+            replace: new Date().toISOString().substring(0, 10),
           },
         ],
       },
